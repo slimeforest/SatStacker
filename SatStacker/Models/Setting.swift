a4ba3e708d0bb7052dd144b7fa1306f6d5ccdd47
zenolook123 <zgedina@yahoo.com>
2023-03-08 23:01:00 -0600

    Initial Setting Model
    created a basic setting model and view controller

---
+++ SatStacker/Models/Setting.swift
@@ -0,0 +1,22 @@
+//
+//  Setting.swift
+//  SatStacker
+//
+//  Created by Zachary Garrison on 3/6/23.
+//
+
+import Foundation
+import UIKit
+class Setting {
+    let settingName1: String
+    let settingIsToggled1: Bool
+    let settingImage1: UIImage
+    
+    
+    init(settingName: String, settingIsToggled: Bool, settingImage: UIImage) {
+        self.settingName1 = settingName
+        self.settingIsToggled1 = settingIsToggled
+        self.settingImage1 = settingImage
+    }
+    
+}
